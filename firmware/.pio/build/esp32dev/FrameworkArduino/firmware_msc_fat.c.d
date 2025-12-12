a1509ea78671160ea08fc0bb408c98735248ce41
Rob Stokes <rob.stokes@superdry.com>
2025-12-12 14:01:54 +0000

    removed .pio folder

--- firmware/.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
- /Users/rob.stokes/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- /Users/rob.stokes/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
--- firmware/.pio/build/esp32dev/lib404/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/lib404/WebServer/detail/mimetable.cpp.o: \
- /Users/rob.stokes/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- /Users/rob.stokes/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- /Users/rob.stokes/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
